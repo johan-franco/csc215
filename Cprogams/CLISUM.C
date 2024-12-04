2f2ae9136a657816c5dd72007ddd840a356f68e5
Johan Franco-Campos <johan.francocampos@gmail.com>
2024-12-03 22:17:44 -0500

    put observations

--- Cprogams/CLISUM.C
+++ Cprogams/CLISUM.C
@@ -5,8 +5,9 @@ char **argv;
 {
     int i, total;
     total = 0;
-    for (i=1; i <= argc; ++i)
-        total = total +  argv[i];
-        printf(total);
-    printf(total);
+    for (i=1; i < argc; ++i)
+        //"every char in an expression is automatically converted to an int" after the atoi
+        total += atoi(argv[i]);
+    //printf needs the %d doesn't work if you simply put total
+    printf("Total calculated: %d",total);
 }
