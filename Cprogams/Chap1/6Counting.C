9c2c2cb06692453e23e01b6ed24f335cdcdb5d4d
Johan Franco-Campos <johan.francocampos@gmail.com>
2024-12-04 09:15:44 -0500

    found out that only newlines seems to be incrementing and * is being outputted

--- Cprogams/Chap1/6Counting.C
+++ Cprogams/Chap1/6Counting.C
@@ -13,25 +13,26 @@ char** argv;
     if (argc != 2) {
         printf("Input of one file needed\n");
         return;
-    }
+        }
     printf("Starting the count!\n");
     infp = fopen(argv[1], "r");
     while ((c = fgetc(infp)) != EOF) {
         if(c == '\n') {
             nl+=1;
-            printf("Incrementing newlines %d", nl);
+            printf("Incrementing newlines %d\n", nl);
         }
         else if (c == " "){
             b+=1;
-            printf("Incrementing blank %d", b);
+            printf("Incrementing blank %d\n", b);
         }
         else if (c == '\t') {
             t+=1;
-            printf("Incrementing tab %d", t)
+            printf("Incrementing tab %d\n", t);
         }
-        printf("Didn't find anything or i can interpret what char is %s", c);
+        printf("Didn't find anything or i can interpret what char is %s\n", c);
     }
     printf("Num of tabs %d \n Num of spaces %d \n Num of newlines %d \n", t,b,nl);
     fclose(infp);
     return 0;
+
 }
