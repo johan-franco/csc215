6e434ddd1991ed0af4e6ac042eb30a47d8459fe5
Johan Franco-Campos <johan.francocampos@gmail.com>
2024-12-03 23:40:20 -0500

    finished struggling

--- Cprogams/CLISUM.C
+++ Cprogams/CLISUM.C
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
-#define EXIT_SUCCESS 0;
-#define EXIT_ERROR 1;
+
 main(argc,argv) 
 int argc;
 char **argv;
 {
-    int i, total, input;
+    int i, j,total, input;
     char *a;
     total = 0;
     for (i=1; i < argc; ++i){
         a = argv[i];
-        for (i=0; i <strlen(a); )
         input = atoi(argv[i]);
-        if (a[0] != '0' && input == 0){
-            printf("OOPS %s is not accepted", a);
-            return EXIT_ERROR;
+        for (j=0; j <strlen(a); ++j){
+            if (a[j] != '0' && input == 0){
+                printf("OOPS %s is not accepted", a);
+                return 0;
             }
-
-        else{
-            total += input;
-            }
+        }
+        total += input;
     }
+    
 
-    printf("Total calculated: %d",total);
+    printf("SUM: %d",total);
     return 0;
 }
