b7e116bbd642c89dc1860d179042aabac226d24a
Johan Franco-Campos <johan.francocampos@gmail.com>
2024-12-03 23:34:00 -0500

    struggling with 0

--- Cprogams/CLISUM.C
+++ Cprogams/CLISUM.C
@@ -1,13 +1,28 @@
 #include <stdio.h>
+
+#define EXIT_SUCCESS 0;
+#define EXIT_ERROR 1;
 main(argc,argv) 
 int argc;
 char **argv;
 {
-    int i, total;
+    int i, total, input;
+    char *a;
     total = 0;
-    for (i=1; i < argc; ++i)
-        //"every char in an expression is automatically converted to an int" after the atoi
-        total += atoi(argv[i]);
-    //printf needs the %d doesn't work if you simply put total
+    for (i=1; i < argc; ++i){
+        a = argv[i];
+        for (i=0; i <strlen(a); )
+        input = atoi(argv[i]);
+        if (a[0] != '0' && input == 0){
+            printf("OOPS %s is not accepted", a);
+            return EXIT_ERROR;
+            }
+
+        else{
+            total += input;
+            }
+    }
+
     printf("Total calculated: %d",total);
+    return 0;
 }
