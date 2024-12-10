134940812bf272283c8f3a7cedfcf7fca690f68d
Johan Franco-Campos <johan.francocampos@gmail.com>
2024-12-10 09:20:33 -0500

    issue with definitng pointer daytab's values

--- Cprogams/Chap1/PointerMonth.C
+++ Cprogams/Chap1/PointerMonth.C
@@ -1,9 +1,4 @@
-int *day_tab[2];
-
-day_tab = {
-    {0, 31, 29, 31, 30, 31,3 0, 31, 31, 30, 31, 30, 31},
-    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-};
+int *day_tab[2]; 
 
 day_of_year(year, month, day)
 int year, month, day;
@@ -24,14 +19,16 @@ int yearday, *pmonth, *pday;
     leap = year%4 == 0 && year %100 != 0 || year%400 == 0;
     for (i = 1; yearday > day_tab[leap][i]; i++)
         yearday -= day_tab[leap][i];
-    return(day);
+    *pmonth = i;
+    *pday = yearday;
 }
 
 main()
 {
+    daytab[0] = {0, 31, 29, 31, 30, 31,30, 31, 31, 30, 31, 30, 31};
+    day_tab[1] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int birthday;
     birthday = day_of_year(2007,1,22);
-    printf("%d", birthday)
-    monthday()
+    printf("Yearday of my birthday: %d", birthday);
 
 }
