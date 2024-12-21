a422cc93f8cf9599068cc364497469c99a3edb9a
Johan Franco-Campos <johan.francocampos@gmail.com>
2024-12-20 22:06:42 -0500

    attempting to test addition

--- BigInt/CALC.C
+++ BigInt/CALC.C
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #define BAD_OP  '\0'
+#include "BIGINT.H"
 /*
 This program implements a three function calculator as the first step to a
 test program for bignum struct.
 */
 int main()
 {
-    int accumulator, input, op;
-    accumulator = 0;
+    struct bigint accumulator;
+    struct bigint input;
+    struct bigint result;
+
+    char buffer[3 * MAX_DIGITS ];
+    char op;
     /* Set op to '+' so first entered value is added to zero. */
+    set_bigint("0", &accumulator);
     op = '+';
     while (op != '=')
     {
+        char input_str[3*MAX_DIGITS];
         printf("Enter value: ");
-        scanf("%d", &input);
+        scanf("%d", input_str);
+        set_bigint(input_str,&input);
+
         switch (op)
         {
             case '+':
-                accumulator += input;
+                Add(&accumulator,&input, &result);
                 break;
             case '-':
                 accumulator -= input;
