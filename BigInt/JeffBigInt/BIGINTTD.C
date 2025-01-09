d5e1ddcc7876005d42dd76e269f99f551e14237a
Johan Franco-Campos <johan.francocampos@gmail.com>
2025-01-09 10:26:39 -0500

    jeff version of bigint

---
+++ BigInt/JeffBigInt/BIGINTTD.C
@@ -0,0 +1,14 @@
+#include "BIGINT.H"
+#include "BDSCTEST.H"
+
+main() {
+    START_TESTING("BIGINTTD.C");
+
+    TEST_CASE("Read and write bigint 1234567") {
+        struct bigint bi;
+        set_bigint("1234567", &bi);
+        ASSERT_STR(get_bigint(bi, "1234567"));
+    }
+
+    END_TESTING();
+}
