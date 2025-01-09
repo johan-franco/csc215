57425fb57926e10e16ed4d52fc3d22d12d35ede4
Johan Franco-Campos <johan.francocampos@gmail.com>
2025-01-09 10:57:40 -0500

    fixed insertion

--- BigInt/JeffBigInt/BIGINTTD.C
+++ BigInt/JeffBigInt/BIGINTTD.C
@@ -7,7 +7,7 @@ main() {
     TEST_CASE("Read and write bigint 1234567") {
         struct bigint bi;
         set_bigint("1234567", &bi);
-        ASSERT_STR(get_bigint(bi) == "1234567");
+        ASSERT_STR(get_bigint(bi), "1234567");
     }
 
     END_TESTING();
